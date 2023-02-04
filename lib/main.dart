f622682b56f6990e6ba721567fb86c27ebb16144
ChoHJin <cjc9739@naver.com>
2023-02-04 17:33:56 +0900

    feat : 하트(favorite)버튼과 리스트 뷰 생성
    생성된 아이템들에 하트버튼을 만들어 하트 버튼을 누르면 리스트 뷰에 저장하여 버튼이 눌린 아이템들만 볼 수 있도록 함.

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
 class RandomWordsState extends State<RandomWords> {
   //Dart 언어에서는 식별자 앞에 '_' 를 붙이면 private 적용이 됨.
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget _buildSuggestions() {
@@ -35,11 +36,25 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title : Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color : alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
@@ -48,11 +63,43 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title : Text('Startup name Generator'),
+        actions: [
+          IconButton(onPressed: _pushSaved, icon: Icon(Icons.list)),
+        ],
       ),
       body : _buildSuggestions()
     );
   }
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+              (WordPair pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+          );
 
+          final divided = ListTile.divideTiles(
+              tiles: tiles,
+              context: context
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title : Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        }
+      ),
+    );
+  }
 
 }
 
